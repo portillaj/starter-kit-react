9b36f23807d654fe7923bdc86f6a1409b56d80c1
portillaj <jason.portilla85@gmail.com>
2018-02-19 16:51:26 -0500

    starter-kit for react-redux projects

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../../style/style.scss';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <h1>Starter Kit with Bootstrap 4</h1>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader', 'eslint-loader']
+    },
+    {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
